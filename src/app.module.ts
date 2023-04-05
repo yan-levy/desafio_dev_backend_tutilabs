4fe490cd6dc28c9b07b1b5e39db0d8d58a88556c
Yan Batista <Yan.Batista@tutiplast.local>
2023-04-05 00:26:13 -0400

    ♻️ refactor(app): removed unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ScriptModule } from './script/script.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot(), AuthModule, ScriptModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
