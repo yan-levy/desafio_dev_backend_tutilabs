cdcd6608aea821689fd68294d7cb642e1e77def8
Yan Batista <Yan.Batista@tutiplast.local>
2023-04-05 00:25:45 -0400

    ✨ feat(swagger): add docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('FTI API')
+    .setDescription('Made by @Tutilabs')
+    .setVersion('1.0')
+    .addTag('FTI')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const theme = new SwaggerTheme('v3');
+  const options = {
+    explorer: true,
+    customCss: theme.getBuffer('dark'),
+    customSiteTitle: 'FTI API Documentation',
+    customfavIcon: './assets/tutilabs.ico',
+  };
+
+  SwaggerModule.setup('api', app, document, options);
+
   await app.listen(process.env.API_PORT);
   Logger.log(`🚀 Listening on PORT ${process.env.API_PORT}`);
   Logger.log(
