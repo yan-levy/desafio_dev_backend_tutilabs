cd7d41a0814e4a0f65c5ae1d6b6d77af6504680a
Yan Batista <Yan.Batista@tutiplast.local>
2023-04-07 16:03:55 -0400

    🔥 fix: remove unused provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ScriptModule } from './script/script.module';
@@ -7,6 +6,6 @@ import { ScriptModule } from './script/script.module';
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, ScriptModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
